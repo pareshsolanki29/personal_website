9301026639101250b36f58ac64da20808f45c980
paresh solanki <bandralites@gmail.com>
2017-02-02 23:03:28 -0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+
+
+	$('#social').hover(function(){
+       console.log("yo");
+        $('.socialMedia').fadeIn(400)
+},
+           function(){
+console.log('out of here')
+        $('.socialMedia').fadeOut(400)
+
+             }
+);
+	$('#myWork').hover(function(){
+		console.log('yoyo');
+		$('.myWork').fadeIn(400)},
+		function(){
+		$('.myWork').fadeOut(400),
+
+			console.log('out of here')
+
+		}
+	);
+
+	
+
+	
+
+
+
+
+});
